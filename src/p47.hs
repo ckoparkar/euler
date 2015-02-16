e7b1f56a9519ca25be054e9dac9378a9b25fc80f
cskksc <ckoparkar@live.in>
2015-02-16 09:33:55 +0530

    p47

---
+++ src/p47.hs
@@ -0,0 +1,22 @@
+-- The first two consecutive numbers to have two distinct prime factors are:
+
+-- 14 = 2 × 7
+-- 15 = 3 × 5
+
+-- The first three consecutive numbers to have three distinct prime factors are:
+
+-- 644 = 2² × 7 × 23
+-- 645 = 3 × 5 × 43
+-- 646 = 2 × 17 × 19.
+
+-- Find the first four consecutive integers to have four distinct prime factors. What is the first of these numbers?
+
+import P3 (factors)
+import Data.List (union, nub)
+import qualified Data.MemoCombinators as Memo
+
+-- allFactors :: Integer -> Int
+allFactors = Memo.integral allFactors'
+  where allFactors' x = map nub [factors x, factors (x+1), factors (x+2), factors (x+3)]
+
+m = [x | x <- [1..], all ((== 4) . length) (allFactors x)]
