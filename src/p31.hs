a03e424e0c4f4c59e9e2379798c8fbd0a7e59ff9
cskksc <ckoparkar@live.in>
2015-02-16 10:37:43 +0530

    faster 31.

--- src/p31.hs
+++ src/p31.hs
@@ -6,10 +6,15 @@
 -- 1×£1 + 1×50p + 2×20p + 1×5p + 1×2p + 3×1p
 -- How many different ways can £2 be made using any number of coins?
 
+import qualified Data.MemoCombinators as Memo
+
 count_change :: Integer -> [Integer] -> Integer
-count_change amt coins
-  | null coins = 0
-  | amt < head coins = count_change amt (tail coins)
-  | amt == head coins = 1 + count_change amt (tail coins)
-  | otherwise = count_change (amt - head coins) coins
-                + count_change amt (tail coins)
+count_change = Memo.memo2 Memo.integral (Memo.list Memo.integral) count_change'
+  where count_change' amt coins
+          | null coins = 0
+          | amt < head coins = count_change amt (tail coins)
+          | amt == head coins = 1 + count_change amt (tail coins)
+          | otherwise = count_change (amt - head coins) coins
+                        + count_change amt (tail coins)
+
+m = count_change 200 [200,100,50,20,10,5,2,1]
