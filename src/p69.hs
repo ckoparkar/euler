1faa602d702103d69b782b5939cf74c31b72ac1c
cskksc <ckoparkar@live.in>
2015-03-06 20:30:41 +0530

    p69

---
+++ src/p69.hs
@@ -0,0 +1,28 @@
+-- Euler's Totient function, φ(n) [sometimes called the phi function], is used to determine the number of numbers less than n which are relatively prime to n. For example, as 1, 2, 4, 5, 7, and 8, are all less than nine and relatively prime to nine, φ(9)=6.
+
+-- n	Relatively Prime	φ(n)	n/φ(n)
+-- 2	1                       1	2
+-- 3	1,2	                2	1.5
+-- 4	1,3	                2	2
+-- 5	1,2,3,4	                4	1.25
+-- 6	1,5	                2	3
+-- 7	1,2,3,4,5,6	        6	1.1666...
+-- 8	1,3,5,7	                4	2
+-- 9	1,2,4,5,7,8	        6	1.5
+-- 10	1,3,7,9	                4	2.5
+-- It can be seen that n=6 produces a maximum n/φ(n) for n ≤ 10.
+
+-- Find the value of n ≤ 1,000,000 for which n/φ(n) is a maximum.
+
+-- Follow the induction case: http://mathworld.wolfram.com/TotientFunction.html
+
+-- Running time: 57.51 secs
+
+import P3(factors)
+import Data.List(nub)
+
+totient :: Integer -> Double
+totient 1 = 1.0
+totient n = (fromIntegral n) * product [1.0 - (1.0 / (fromIntegral p)) | p <- nub $ factors n]
+
+m = snd $ maximum $ map (\n -> ((fromIntegral n) / totient n,n)) [2..1000000]
