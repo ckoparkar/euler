e4b3a5f04196ba1a0da001ae399ef80c1bd3d4b0
cskksc <ckoparkar@live.in>
2015-03-16 09:01:32 +0530

    Sort on user provided predicate.

--- src/merge_sort.hs
+++ src/merge_sort.hs
@@ -1,14 +1,18 @@
-mergeSort :: Ord a => [a] -> [a]
-mergeSort [] = []
-mergeSort [x] = [x]
-mergeSort xs = merge (mergeSort as) (mergeSort bs)
+mergeSort :: (a -> a -> Bool) -> [a] -> [a]
+mergeSort f [] = []
+mergeSort f [x] = [x]
+mergeSort f xs = merge f (mergeSort f as) (mergeSort f bs)
   where (as, bs) = splitAt (length xs `div` 2) xs
 
-merge :: Ord a => [a] -> [a] -> [a]
-merge as [] = as
-merge [] bs = bs
-merge (a:as) (b:bs) =
-  if a < b then
-    a : merge as (b:bs)
-  else
-    b : merge (a:as) bs
+merge :: (t -> t -> Bool) -> [t] -> [t] -> [t]
+merge f as [] = as
+merge f [] bs = bs
+merge f (a:as) (b:bs)
+  | f a b = a : merge f as (b:bs)
+  | otherwise = b : merge f (a:as) bs
+
+m = mergeSort (<) [100,99..1]
+n = mergeSort (>) [1..100]
+o = mergeSort (\a b -> length a < length b) ["aaaa","aaa","aa","a"]
+p = mergeSort (\a b -> length a < length b) [[4444], [333],[22], [1]]
+q = mergeSort (\a b -> sum a < sum b) [[1111], [222],[33], [4]]
