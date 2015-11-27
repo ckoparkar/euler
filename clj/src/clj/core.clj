fa3223aec1d0808c4462b51f24ab7e0c96bd11f8
cskksc <ckoparkar@live.in>
2015-11-27 17:34:42 +0530

    clj: p4

--- clj/src/clj/core.clj
+++ clj/src/clj/core.clj
@@ -20,9 +20,19 @@
      (> (Math/pow k 2) n) n ;; means n is a prime number
      :else (least-prime-factor (inc k) n))))
 
-(defn factors
+(defn prime-factors
   "Returns list of prime factors of n"
   [n]
   (let [p (least-prime-factor n)]
     (if (= n 1) nil
-        (cons p (lazy-seq (factors (/ n p)))))))
+        (cons p (lazy-seq (prime-factors (/ n p)))))))
+
+(defn palindrome?
+  "Returns if a n is palindrome"
+  [n]
+  (= (reverse (reverse n)) (reverse n)))
+
+(defn digits
+  "Returns list of digits of a int"
+  [n]
+  (map #(Character/getNumericValue %) (str n)))
