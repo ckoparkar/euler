e9419743c4694c14652a542ce89cd91cdd9d4a10
cskksc <ckoparkar@live.in>
2015-11-28 15:02:34 +0530

    clj: add problem statements

--- clj/src/clj/p5.clj
+++ clj/src/clj/p5.clj
@@ -1,6 +1,10 @@
 (ns clj.p5
   (:use [clj.core :only (lcm)]))
 
+"2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?"
+
 (defn soln
   [n]
   (reduce lcm (range 1 n)))
--- clj/src/clj/p7.clj
+++ clj/src/clj/p7.clj
@@ -1,6 +1,10 @@
 (ns clj.p7
   (:use [clj.core :only (primes)]))
 
+"By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?"
+
 (defn soln
   [n]
   (nth primes n))
